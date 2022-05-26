2e642fd2b43dad928cf249284ca788986032f3b8
Thiago Ferreira <thiago.fvs@gmail.com>
2022-05-26 00:08:34 -0300

    hr-payroll created

---
+++ hr-pyroll/src/main/java/com/tfs/pyroll/HrPyrollApplication.java
@@ -0,0 +1,13 @@
+package com.tfs.pyroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPyrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPyrollApplication.class, args);
+	}
+
+}
---
+++ hr-pyroll/src/test/java/com/tfs/pyroll/HrPyrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfs.pyroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPyrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
