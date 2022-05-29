54a49fe89bce1ea32981713c9556240635bc7cbf
Thiago Ferreira <thiago.fvs@gmail.com>
2022-05-28 23:38:55 -0300

    RestTemplate

--- hr-pyroll/src/main/java/com/tfs/hrpayroll/entities/Worker.java
+++ hr-pyroll/src/main/java/com/tfs/hrpayroll/entities/Worker.java
@@ -7,19 +7,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class Payment implements Serializable {
+@NoArgsConstructor
+public class Worker implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
+	private Long id;
+	
 	private String name;
-
+	
 	private Double dailyIncome;
-
-	private Integer days;
-
-	public double getTotal() {
-		return days * dailyIncome;
-	}
 
 }
--- hr-pyroll/src/main/java/com/tfs/hrpayroll/services/PaymentService.java
+++ hr-pyroll/src/main/java/com/tfs/hrpayroll/services/PaymentService.java
@@ -1,14 +1,31 @@
 package com.tfs.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.tfs.hrpayroll.entities.Payment;
+import com.tfs.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
-	public Payment getPaymant(long workId, int days) {
-		return new Payment("Bob", 200.00, days);
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public Payment getPaymant(long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
