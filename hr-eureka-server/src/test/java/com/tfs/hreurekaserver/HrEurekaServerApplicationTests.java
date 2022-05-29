1a8bdad4cf75764eefa0e2c6c6091d4ac7371c70
Thiago Ferreira <thiago.fvs@gmail.com>
2022-05-29 13:35:53 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/tfs/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfs.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
