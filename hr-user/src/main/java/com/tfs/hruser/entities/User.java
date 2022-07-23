bda091303fd5a533838584b5b8146b485da099ee
Thiago Ferreira <thiago.fvs@gmail.com>
2022-07-23 19:49:12 -0300

    Database seeding

--- hr-user/src/main/java/com/tfs/hruser/entities/User.java
+++ hr-user/src/main/java/com/tfs/hruser/entities/User.java
@@ -24,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id	
@@ -37,7 +38,7 @@ public class User implements Serializable {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_use_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public Set<Role> getRoles(){
