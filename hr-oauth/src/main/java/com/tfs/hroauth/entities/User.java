d23e517bdc5c4a9fbac93f9ba3931205e5be46ab
Thiago Ferreira <thiago.fvs@gmail.com>
2022-07-24 16:04:55 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/tfs/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/tfs/hroauth/entities/User.java
@@ -1,8 +1,14 @@
 package com.tfs.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
@@ -27,6 +33,37 @@ public class User implements Serializable {
 	
 	public Set<Role> getRoles(){
 		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
